1f97f538178e9d604f08909a9380ddda39e1cd89
Thoof <jason.heraldo@gmail.com>
2016-05-09 20:16:46 -0700

    Basic card prefab
    Added a basic card button prefab with changeable name/description.
    Currently shown in CharacterCustomization scene

---
+++ Assets/MenuScripts/CharacterCustomization.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCustomization : MonoBehaviour {
+    GameObject cardPrefab;
+    GameObject myCanvas;
+	// Use this for initialization
+	void Start () {
+        myCanvas = GameObject.Find("Canvas");
+	    cardPrefab = Instantiate(Resources.Load("Prefabs/Card")) as GameObject;
+        cardPrefab.transform.SetParent(myCanvas.transform, false);
+        CardUI uiComponent = cardPrefab.GetComponent<CardUI>();
+
+        uiComponent.setName("Test name");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/UI/CardUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CardUI : MonoBehaviour {
+    Text cardName;
+    Text cardDescription;
+    Button button;
+
+    GameObject cardPrefab;
+
+	// Use this for initialization
+	void Awake () {
+
+        Text[] textComponents = GetComponentsInChildren<Text>();
+        cardName = textComponents[0];
+        cardDescription = textComponents[1];
+
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(
+            () =>
+            {
+                SceneManager.LoadScene("CardSelect");
+            });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void setName(string name)
+    {
+        cardName.text = name;
+    }
+
+    public void setDescription(string description)
+    {
+        cardDescription.text = description;
+    }
+}
