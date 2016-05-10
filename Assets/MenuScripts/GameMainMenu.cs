d38ef7023aee853c4e278f8d87bf939a61d761f6
Thoof <jason.heraldo@gmail.com>
2016-05-09 19:23:04 -0700

    Added blank scenes + MainMenu + GameMainMenu
    Set up buttons for main menu & game main menu and made them go to the
    appropriate scene

---
+++ Assets/MenuScripts/GameMainMenu.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMainMenu : MonoBehaviour {
+
+    public Button mNextLevelButton;
+    public Button mCustomizeButton;
+    public Button mSaveButton;
+    public Button mMenuButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        mNextLevelButton = GameObject.Find("NextLevel").GetComponent<Button>();
+        mCustomizeButton = GameObject.Find("Customize").GetComponent<Button>();
+        mSaveButton = GameObject.Find("Save").GetComponent<Button>();
+        mMenuButton = GameObject.Find("MainMenu").GetComponent<Button>();
+
+        mNextLevelButton.onClick.AddListener(
+            () =>
+            {
+                LoadScene("LevelScene");
+            });
+
+        mCustomizeButton.onClick.AddListener(
+            () =>
+            {
+                //Load game then go to gamemainmenu
+                LoadScene("CharCustomization");
+            });
+
+        mSaveButton.onClick.AddListener(
+            () =>
+            {
+                //Save here
+            });
+
+        mMenuButton.onClick.AddListener(
+            () =>
+            {
+                LoadScene("MainMenu");
+            });
+
+    }
+
+    void LoadScene(string theLevel)
+    {
+        SceneManager.LoadScene(theLevel);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/MenuScripts/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public Button mStartButton;
+    public Button mLoadButton;
+    public Button mCreditsButton;
+    public Button mQuitButton;
+
+    // Use this for initialization
+    void Start () {
+        mStartButton = GameObject.Find("NewGame").GetComponent<Button>();
+        mLoadButton = GameObject.Find("Load Game").GetComponent<Button>();
+        mCreditsButton = GameObject.Find("Credits").GetComponent<Button>();
+        mQuitButton = GameObject.Find("Quit").GetComponent<Button>();
+
+        mStartButton.onClick.AddListener(
+            () =>
+            {
+                LoadScene("GameMainMenu");
+            });
+
+        mLoadButton.onClick.AddListener(
+            () =>
+            {
+                //Load game then go to gamemainmenu
+                LoadScene("GameMainMenu");
+            });
+
+        mCreditsButton.onClick.AddListener(
+            () =>
+            {
+                LoadScene("Credits");
+            });
+
+        mQuitButton.onClick.AddListener(
+            () =>
+            {
+                Application.Quit();
+            });
+
+    }
+
+    void LoadScene(string theLevel)
+    {
+        SceneManager.LoadScene(theLevel);
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
