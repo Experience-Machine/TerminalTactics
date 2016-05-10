eb59ee91498498f841c01e955586ebc1fb48857c
Thoof <jason.heraldo@gmail.com>
2016-05-09 19:28:37 -0700

    Credits scene

---
+++ Assets/MenuScripts/Credits.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public Button mReturnButton;
+    // Use this for initialization
+    void Start()
+    {
+        mReturnButton = GameObject.Find("CreditsButton").GetComponent<Button>();
+
+        mReturnButton.onClick.AddListener(
+            () =>
+            {
+                LoadScene("MainMenu");
+            });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LoadScene(string theLevel)
+    {
+        SceneManager.LoadScene(theLevel);
+        //        FirstGameManager.TheGameState.SetCurrentLevel(theLevel);
+
+    }
+}
