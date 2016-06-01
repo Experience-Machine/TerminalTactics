1de7e0636d775c8ab705e136e84b2fb50191122a
Scott Fergsuon <sfthe5@gmail.com>
2016-06-01 16:53:03 -0700

    Fixed camera jitter, added wasd and arrow keys

--- Assets/Level/Camerabehaviour.cs
+++ Assets/Level/Camerabehaviour.cs
@@ -9,7 +9,16 @@ public class Camerabehaviour : MonoBehaviour {
     private Vector3 mRightDirection = Vector3.right;
 
     private float cameraMaxSize = 7f;
-    private float cameraMinSize = 4f;
+    private float cameraMinSize = 2f;
+
+    float xPos;
+    float yPos;
+
+    float xBoundLeft = 10.0f;
+    float xBoundRight = 20.0f;
+
+    float yBoundTop = 12.5f;
+    float yBoundBottom = 6.0f;
 
     // Use this for initialization
     void Start () {
@@ -19,19 +28,36 @@ public class Camerabehaviour : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
     
+        // get the camera coordinates
+        xPos = Camera.main.gameObject.transform.position.x;
+        yPos = Camera.main.gameObject.transform.position.y;
+
+        // mouse pan
         panRight();
         panLeft();
         panUp();
         panDown();
-        zoom();
+
+        // wasd ad arrow key support
+        W();
+        A();
+        S();
+        D();
+
+        // zoom support
+        zoom(); 
+ 
     }
 
     bool panRight() {
 
         if (Input.mousePosition.x >= Screen.width - mDelta) // hitting the right
         {
-            // Move the camera
-            transform.position += mRightDirection * Time.deltaTime * mSpeed;
+            if (xPos < xBoundRight)
+            {
+                // Move the camera
+                transform.position += mRightDirection * Time.deltaTime * mSpeed;
+            }
         }
 
         return false;
@@ -41,7 +67,10 @@ public class Camerabehaviour : MonoBehaviour {
 
         if (Input.mousePosition.x <= 0 + mDelta) // hitting the left
         {
-            transform.position -= mRightDirection * Time.deltaTime * mSpeed;
+            if (xPos > xBoundLeft)
+            {
+                transform.position -= mRightDirection * Time.deltaTime * mSpeed;
+            }
         }
 
         return false;
@@ -53,8 +82,11 @@ public class Camerabehaviour : MonoBehaviour {
 
         if (Input.mousePosition.y > Screen.height - mDelta) // hitting the top
         {
-            newpos.y += Time.deltaTime * mSpeed;
-            transform.position = newpos;
+            if (yPos < yBoundTop)
+            {
+                newpos.y += Time.deltaTime * mSpeed;
+                transform.position = newpos;
+            }
         }
 
         return false;
@@ -66,8 +98,64 @@ public class Camerabehaviour : MonoBehaviour {
 
         if (Input.mousePosition.y <= 0 + mDelta) // hitting the bottom
         {
-            newpos.y -= Time.deltaTime * mSpeed;
-            transform.position = newpos;
+            if (yPos > yBoundBottom)
+            {
+                newpos.y -= Time.deltaTime * mSpeed;
+                transform.position = newpos;
+            }
+        }
+
+        return false;
+    }
+
+    bool W() {
+        //yPos = Camera.main.gameObject.transform.position.y;
+        //Debug.Log(yPos);
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            if (yPos < yBoundTop)
+            {
+                transform.Translate(new Vector3(0, mSpeed * Time.deltaTime, 0));
+            }
+        }
+
+        return false;
+    }
+
+    bool A()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (xPos > xBoundLeft)
+            {
+                transform.Translate(new Vector3(-mSpeed * Time.deltaTime, 0, 0));
+            }
+        }
+        return false;
+    }
+
+    bool S()
+    {
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            if (yPos > yBoundBottom) {
+                {
+                    transform.Translate(new Vector3(0, -mSpeed * Time.deltaTime, 0));
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool D()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (xPos < xBoundRight)
+            {
+                transform.Translate(new Vector3(mSpeed * Time.deltaTime, 0, 0));
+            }
         }
 
         return false;
@@ -77,11 +165,11 @@ public class Camerabehaviour : MonoBehaviour {
     {
         float change = Input.GetAxis("Mouse ScrollWheel");
 
-        if (change > 0f || Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadPlus)) 
+        if (change < 0f || Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadPlus)) 
         {
             if (Camera.main.orthographicSize + 0.5f <= cameraMaxSize)
                 Camera.main.orthographicSize += 0.5f;
-        } else if (change < 0f || Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadMinus)) //Actually the plus key (non shift)
+        } else if (change > 0f || Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadMinus)) //Actually the plus key (non shift)
         {
             if (Camera.main.orthographicSize - 0.5f >= cameraMinSize) 
                 Camera.main.orthographicSize -= 0.5f;
